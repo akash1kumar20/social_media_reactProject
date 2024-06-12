4d69f101cc39a33f9af79adbb050ce9fdde7cbb2
akash1kumar20 <akashkumar2101sm@gmail.com>
2024-06-13 00:03:10 +0530

    Inital Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-center">
+      <h1 className="font-semibold text-3xl pt-6">Our social media project</h1>
+    </div>
+  );
+};
+
+export default App;
